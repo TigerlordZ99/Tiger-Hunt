c3667ecffd6cdc4e2213da0aa4d8c19add060e0d
Madhav Ramakrishnan <madhavramakrishnan99@gmail.com>
2025-02-14 00:50:22 -0800

    Play function updated

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -4,15 +4,89 @@ class Play extends Phaser.Scene{
     }
     
     create(){
+        //SETUP
         this.sky = this.add.tileSprite(0, 0, 640, 480, "sky").setOrigin(0, 0)
         this.physics.world.setBounds(0, 0, 640, 480)
-        this.p1Tiger = new Tiger(this, 60, 370, "tiger").setOrigin(0, 0)
         keyJUMP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
         keyATTACK = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER)
+        this.p1Tiger = new Tiger(this, 60, 370, "tiger").setOrigin(0, 0)
+        this.physics.add.collider(this.p1Tiger, this.ground)
+        
+        //HEALTH
+        this.maxHealth = 3
+        this.health = this.maxHealth
+        this.healthIcons = []
+        for (let i = 0; i < this.maxHealth; i++) {
+            let heart = this.add.image(540 + i * 30, 40, "health")
+            this.healthIcons.push(heart)
+        }
+
+        //FOOD
+        this.foodGroup = this.physics.add.group()
+        this.spawnFood()
+
+        this.time.addEvent({
+            delay: 10000,
+            callback: this.spawnFood,
+            callbackScope: this,
+            loop: true
+        })
+
+        this.physics.add.overlap(this.p1Tiger, this.foodGroup, this.collectFood, null, this)
+
+        //DAMAGE
+        this.time.addEvent({
+            delay: 10000,
+            callback: this.decreaseHealth,
+            callbackScope: this,
+            loop: true
+        })
+
+        console.log("Health is " + this.health)
     }
 
     update(){
         this.sky.tilePositionX += 2
         this.p1Tiger.update()
+
+        this.foodGroup.children.each((food) => {
+            food.x -= 2
+            if (food.x < 15) {
+                food.destroy()
+            }
+        }, this)
+    }
+
+    decreaseHealth(){
+        if (this.health > 0) {
+            this.health -= 1
+            console.log("Health is " + this.health)
+            this.updateHealthIcons()
+        }
+    }
+
+    spawnFood(){
+        let x = Phaser.Math.Between(700, 900)
+        let y = Phaser.Math.Between(300, 400)
+        let food = this.foodGroup.create(x, y, "food")
+        food.setImmovable(true)
+        food.body.allowGravity = false
+        food.setCollideWorldBounds(true)
+    }
+
+    collectFood(tiger, food) {
+        if (!food || !food.active) return
+        if (this.health < this.maxHealth) {
+            this.health++
+            this.updateHealthIcons()
+            console.log("Health is " + this.health)
+        }
+        food.destroy()
+    }
+    
+    updateHealthIcons() {
+        for (let i = 0; i < this.maxHealth; i++) {
+            this.healthIcons[i].setVisible(i < this.health)
+        }
     }
 }
